4b41e045663deeec011ad81ce18c9eb03a4a5b5c
Tomi Bedini <bedinitomas@gmail.com>
2022-04-20 23:57:15 -0300

    Clases nuevas
    Me falta terminar el tema de tickets

---
+++ src/Clases/FormularioDeBusqueda.java
@@ -0,0 +1,28 @@
+package Clases;
+
+public class FormularioDeBusqueda {
+	private String locacion;
+	private double remuneracion;
+	private String cargaHoraria;
+	private String tipoPuestoLaboral;
+	private int edad;
+	private String experienciaPrevia;
+	private String estudiosCursados;
+	
+	
+	public FormularioDeBusqueda(String locacion, double remuneracion, String cargaHoraria, String tipoPuestoLaboral,
+			int edad, String experienciaPrevia, String estudiosCursados) {
+		super();
+		this.locacion = locacion;
+		this.remuneracion = remuneracion;
+		this.cargaHoraria = cargaHoraria;
+		this.tipoPuestoLaboral = tipoPuestoLaboral;
+		this.edad = edad;
+		this.experienciaPrevia = experienciaPrevia;
+		this.estudiosCursados = estudiosCursados;
+	}
+	
+	
+	
+
+}
