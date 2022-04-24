00f57f4834acf2a2e63abf60433ba277b21de822
Tomi Bedini <bedinitomas@gmail.com>
2022-04-24 18:28:45 -0300

    Cambios a clase Empleado
    :))

--- src/usuariosDelSistema/Empleado.java
+++ src/usuariosDelSistema/Empleado.java
@@ -9,7 +9,8 @@ public class Empleado extends Usuario{
 	private int telefono;
 	private int edad;
 	private String ciudad;
-	TicketEmpleado ticket;
+	private TicketEmpleado ticket;
+	private Empleador empleadorSeleccionado;
 
 
 	public Empleado(String usuario, String contrasenia, String nya, int dni, int telefono, int edad, String ciudad) {
@@ -24,4 +25,57 @@ public class Empleado extends Usuario{
 	public void setTicket(TicketEmpleado ticket) {
 		this.ticket = ticket;
 	}
+
+	public String getNya() {
+		return nya;
+	}
+
+	public void setNya(String nya) {
+		this.nya = nya;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public Empleador getEmpleadorSeleccionado() {
+		return empleadorSeleccionado;
+	}
+
+	public void setEmpleadorSeleccionado(Empleador empleadorSeleccionado) {
+		this.empleadorSeleccionado = empleadorSeleccionado;
+	}
+
+	public TicketEmpleado getTicket() {
+		return ticket;
+	}
+	
 }
