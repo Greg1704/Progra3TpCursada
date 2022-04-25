7375fe3cb51c659b92a26b8e81e154752da14ab5
Tomi Bedini <bedinitomas@gmail.com>
2022-04-25 10:51:52 -0300

    cambios sobre clase sistema

--- src/usuariosDelSistema/Sistema.java
+++ src/usuariosDelSistema/Sistema.java
@@ -4,52 +4,41 @@ import java.util.ArrayList;
 import armaTickets.*;
 
 public class Sistema {
-    private static Sistema instancia=null;
-    private ArrayList<Usuario> usuarios=new ArrayList<Usuario>();
+	private static Sistema instancia = null;
+	private ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+	private ArrayList<Empleador> empleadores = new ArrayList<Empleador>();
 
+	private Sistema() {
+		System.out.println("Faltan cosas");
+	}
 
-    private Sistema(){
-     System.out.println("Faltan cosas");
-    }
+	public static Sistema getInstancia() {
+		if (instancia == null)
+			instancia = new Sistema();
+		return instancia;
+	}
 
-    public static Sistema getInstancia(){
-        if(instancia == null)
-            instancia=new Sistema();
-        return instancia;
-    }
-    
-   public void agregarUsuario(Usuario user){
-       this.usuarios.add(user);
-   }
-   
-   public void eliminarUsuario(Usuario user){
-      this.usuarios.remove(user);
-  }
+	
+	
+	
 
-   public ArrayList<Usuario> getUsuarios(){
-      return usuarios;
-  }
+	public double comisionEmpleadoPretenso(Empleado empleado) { // determino el porcentaje que le cobro de comision y
+																// calculo el monto a cobrar
+		String tpl = empleado.getEmpleadorSeleccionado().getTicketEmpleador().getFormularioDeBusqueda()
+				.getPuestoLaboral().getTipoPuestoLaboral();// cambien esto hdps
+		double porc;
 
+		if (tpl.equalsIgnoreCase("Junior"))
+			porc = 0.8;
+		else if (tpl.equalsIgnoreCase("Senior"))
+			porc = 0.9;
+		else
+			porc = 1;
+		porc -= 0.01 * empleado.getPuntaje();
+		return empleado.getTicket().getFormularioDeBusqueda().getRemuneracion().getMonto() * porc;
 
-public void comisionEmpleadoPretenso(Empleado empleado){ //determino el porcentaje que le cobro de comision y calculo el monto a cobrar
- String tpl=empleado.getEmpleadorSeleccionado().getTicketEmpleador().getFormularioDeBusqueda().getPuestoLaboral().getTipoPuestoLaboral();//cambien esto hdps
- double porc,comision;
+	}
 
- if(tpl.equals("Junior"))
-   porc=0.8;
-  else
-   if(tpl.equals("Senior")) 
-    porc=0.9;
-    else
-     porc=1;
-  porc-=0.01*empleado.getPuntaje();
-  comision=empleado.getTicket().getFormularioDeBusqueda().getRemuneracion().getMonto()*porc;  
-
-}
-
-
-
-  
 }
 
 /*
