de966c1cd491b2162531260c27f106a4a9ef28ba
Tomi Bedini <bedinitomas@gmail.com>
2022-04-26 02:01:26 -0300

    :)

--- src/usuariosDelSistema/Sistema.java
+++ src/usuariosDelSistema/Sistema.java
@@ -13,7 +13,7 @@ public class Sistema {
 		System.out.println("Faltan cosas");
 	}
 
-	public static Sistema getInstancia() {
+	private static Sistema getInstancia() {
 		if (instancia == null)
 			instancia = new Sistema();
 		return instancia;
