068f4f4c041bad1478bc169fb1dea280976ad453
mostri28 <mateoetchepare@gmail.com>
2022-05-08 18:19:29 -0300

    javadoc 195 files
    disfruten panas

--- src/usuariosDelSistema/RubroFactory.java
+++ src/usuariosDelSistema/RubroFactory.java
@@ -1,12 +1,15 @@
 package usuariosDelSistema;
 
-/**
- * Clase en la que se aplica patrón factory para poder obtener una nueva instancia de rubro específico del empleador.
- * 
- * 
- */ 
 
 public class RubroFactory {
+	
+	/**
+	 * Clase en la que se aplica patrón factory para poder obtener una nueva instancia de rubro específico del empleador. <br>
+	 * @param rubro: El atributo rubro es un string que pasa el rubro al que se dedica el empleador <br>
+	 * PreCond: rubro debe ser un atributo válido: ComercioInternacional ó ComercioLocal ó Salud.
+	 * 
+	 */ 
+	
     public RubroDecorator getRubro(String rubro, Empleador empleador){
         if(rubro == null)
             return null;
