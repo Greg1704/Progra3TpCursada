78cd3daeadc731f968659022665804be04083bd1
Tomi Bedini <bedinitomas@gmail.com>
2022-06-02 12:57:09 -0300

    ultimas anotaciones

--- src/ticketSimplificado/TicketSimplificado.java
+++ src/ticketSimplificado/TicketSimplificado.java
@@ -4,4 +4,6 @@ public class TicketSimplificado {
 //habria que agregar 2 strings: rubro, y locacion. Con eso aplicar los decorator y agregarle las funcionalidades
 //a su vez hay que aplicar metodo run, para asi correrlo como un thread, podemos extender de clase thread o implementar
 //la interfaz runneable.
+//o puede ser un recurso compartido por el que compiten los thread que resultan ser los empleados
+//hay que aplicar a su vez observer observador
 }
