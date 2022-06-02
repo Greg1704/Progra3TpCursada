c51f05159dbf719cb8121ce6c4bbc8e34f69c586
Tomi Bedini <bedinitomas@gmail.com>
2022-06-02 12:48:00 -0300

    anotaciones en ticket simplificado

---
+++ src/ticketSimplificado/TicketSimplificado.java
@@ -0,0 +1,7 @@
+package ticketSimplificado;
+
+public class TicketSimplificado {
+//habria que agregar 2 strings: rubro, y locacion. Con eso aplicar los decorator y agregarle las funcionalidades
+//a su vez hay que aplicar metodo run, para asi correrlo como un thread, podemos extender de clase thread o implementar
+//la interfaz runneable.
+}
