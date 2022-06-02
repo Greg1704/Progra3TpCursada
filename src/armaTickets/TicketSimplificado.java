ef51cbbbc922a15910071c134113b2084fdfe777
Tomi Bedini <bedinitomas@gmail.com>
2022-06-02 11:27:11 -0300

    .

---
+++ src/armaTickets/TicketSimplificado.java
@@ -0,0 +1,10 @@
+package armaTickets;
+
+public class TicketSimplificado {
+	private String tipoDeTrabajo;
+	private String locacion;
+	
+	
+	
+	
+}
--- src/ticketSimplificado/TicketSimplificado.java
+++
@@ -1,5 +0,0 @@
-package ticketSimplificado;
-
-public class TicketSimplificado {
-
-}
