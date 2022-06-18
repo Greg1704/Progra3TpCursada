faad14e59e34b3975307f3ee3cb30b46facf8732
Greg1704 <70973430+Greg1704@users.noreply.github.com>
2022-06-18 18:46:51 -0300

    cambiazo

--- src/vista/IVista.java
+++ src/vista/IVista.java
@@ -1,12 +1,14 @@
 package vista;
 
+import java.awt.event.ActionListener;
+
 public interface IVista {
 	
 	public void setControlador(Controlador c);
 	public void addActionListener(ActionListener listener);
-	static final String añadeEmpleado = "Agregar nuevo empleado";
-	static final String añadeEmpleador = "Agregar nuevo empleador";
-	static final String añadeAgencia = "Agregar nueva agencia"; // esto es un singleton
+	static final String aniadeEmpleado = "Agregar nuevo empleado";
+	static final String aniadeEmpleador = "Agregar nuevo empleador";
+	static final String aniadeAgencia = "Agregar nueva agencia"; // esto es un singleton
 	static final String eligeTicket = "Elegir ticket";
 	static final String siguienteTicket = "Siguiente ticket";
 }
