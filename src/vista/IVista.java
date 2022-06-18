5d96c3f116fc7efe07f3f9247cb7ba5eac925adb
mostri28 <mateoetchepare@gmail.com>
2022-06-18 01:34:36 -0300

    Update IVista.java

--- src/vista/IVista.java
+++ src/vista/IVista.java
@@ -2,6 +2,11 @@ package vista;
 
 public interface IVista {
 	
-	
-	void addActionListener(ActionListener listener);
+	public void setControlador(Controlador c);
+	public void addActionListener(ActionListener listener);
+	static final String añadeEmpleado = "Agregar nuevo empleado";
+	static final String añadeEmpleador = "Agregar nuevo empleador";
+	static final String añadeAgencia = "Agregar nueva agencia"; // esto es un singleton
+	static final String eligeTicket = "Elegir ticket";
+	static final String siguienteTicket = "Siguiente ticket";
 }
