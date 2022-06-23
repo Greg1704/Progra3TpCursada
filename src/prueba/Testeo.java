b4615fff5d677ee4820448acad26d8729d01b2fd
Tomi Bedini <bedinitomas@gmail.com>
2022-06-23 10:52:46 -0300

    .
    nada mas les puse comillas al dni en prueba abz

--- src/prueba/Testeo.java
+++ src/prueba/Testeo.java
@@ -24,19 +24,19 @@ public class Testeo {
 		EmpleadorFisico em3;
 		
 
-		e1 = new Empleado("Juanu","contrasenia","Juan pedro Garcia",43322122,22345654,25,"Mar del Plata");
+		e1 = new Empleado("Juanu","contrasenia","Juan pedro Garcia","43322122",22345654,25,"Mar del Plata");
 		e1.crearTicket("HomeOffice",1,45000,1,"Completa",1,"Junior",1,25,1,"Media",1,"Secundario",1);
 
-		e2 = new Empleado("LauraX","contrasenia123","Laura perez",34556543,223556677,40,"Mar del Plata");
+		e2 = new Empleado("LauraX","contrasenia123","Laura perez","34556543",223556677,40,"Mar del Plata");
 		e2.crearTicket("Presencial",1,800000,1,"Completa",1,"Senior",1,40,1,"Media",1,"Secundario",1);
 
-		e3 = new Empleado("Pedrito45","MamayPapa324","Pedro Fernandez",36789546,223456765,32,"La Plata");
+		e3 = new Empleado("Pedrito45","MamayPapa324","Pedro Fernandez","36789546",223456765,32,"La Plata");
 		e3.crearTicket("Presencial",1,130000,1,"Extendida",1,"Managment",1,32,1,"Mucha",1,"Terciario",1);
 
-		e4 = new Empleado("Carla32","Michifus","Carla Lopez",42654839,223567543,23,"CABA");
+		e4 = new Empleado("Carla32","Michifus","Carla Lopez","42654839",223567543,23,"CABA");
 		e4.crearTicket("Presencial",1,45000,1,"Media",1,"Junior",1,25,1,"Media",1,"Secundario",1);
 
-		e5 = new Empleado("DarthVader78","YoSoyTuPadre","Cacho Enriquez",32456765,482565,42,"Mar Del Plata");
+		e5 = new Empleado("DarthVader78","YoSoyTuPadre","Cacho Enriquez","32456765",482565,42,"Mar Del Plata");
 		e5.crearTicket("Indistinto",1,45000,1,"Media",1,"Junior",1,25,1,"Media",1,"Secundario",1);
 
 		em1 = new EmpleadorJuridico("CocaCola Inc","PepsiNoExistis","Coca Cola Incorporation","ComercioInternacional");
